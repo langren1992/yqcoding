09a7401acaf02cc395170e2827ef89226437a6ef
zengrui <820318767@qq.com>
2020-06-13 12:14:25 +0800

    初始化代码

---
+++ admin/src/main/java/com/anserx/yqcoding/AdminApplication.java
@@ -0,0 +1,15 @@
+package com.anserx.yqcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class AdminApplication {
+    public static void main( String[] args ) {
+        SpringApplication.run(AdminApplication.class);
+    }
+}
---
+++ mq/src/main/java/com/anserx/yqcoding/MqApplication.java
@@ -0,0 +1,12 @@
+package com.anserx.yqcoding;
+
+/**
+ * Hello world!
+ *
+ */
+public class MqApplication {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
